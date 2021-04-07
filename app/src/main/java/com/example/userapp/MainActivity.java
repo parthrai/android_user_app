eb3edbf86ac6f4d911e6d5d8a09860026f63f160
Parth Rai <parthrai2@gmail.com>
2021-04-07 18:15:32 -0400

    volley test

--- app/src/main/java/com/example/userapp/MainActivity.java
+++ app/src/main/java/com/example/userapp/MainActivity.java
@@ -4,15 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText txtEmail;
     EditText txtPassword;
     TextView lblMsg;
+    private RequestQueue mQueue;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void btnLoginClick(View v){
+
 
         if(txtEmail.getText().toString().equals("user@valid.com") && txtPassword.getText().toString().equals("testpass")){
             // show a success msg
@@ -43,5 +59,47 @@ public class MainActivity extends AppCompatActivity {
 
         Intent registerIntent = new Intent(this, RegisterActivity.class);
         startActivity(registerIntent);
+    }
+
+    public void json(View v){
+
+        mQueue = Volley.newRequestQueue(this);
+
+        String url = "https://jsonplaceholder.typicode.com/todos";
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d("@@@##$#%%-", "ResponseCheck :");
+
+                        try {
+
+                            JSONArray jsonarray;
+                                jsonarray = new JSONArray(response);
+                                for (int i = 0; i < jsonarray.length(); i++) {
+                                    JSONObject mJsonObject = jsonarray.optJSONObject(i);
+                                    Log.d("OutPut", mJsonObject.optString("transactionId"));
+                                }
+                        } catch (JSONException e) {
+                            Log.d("$$$$$4","here");
+
+                            e.printStackTrace();
+                            Log.d("$$$$$4",response.toString());
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+                Log.d("$$$$$4","response.toString()");
+
+            }
+        });
+
+
+        mQueue.add(request);
+
     }
 }
