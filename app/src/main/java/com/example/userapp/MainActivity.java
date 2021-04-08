41b17c1683abedffc882248a812a21649ae53e0a
Parth Rai <parthrai2@gmail.com>
2021-04-08 19:58:51 -0400

    android user app

--- app/src/main/java/com/example/userapp/MainActivity.java
+++ app/src/main/java/com/example/userapp/MainActivity.java
@@ -4,9 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,24 +36,61 @@ public class MainActivity extends AppCompatActivity {
         lblMsg = findViewById(R.id.lblMsg);
     }
 
-    public void btnLoginClick(View v){
+    public void btnLoginClick(View v) throws JSONException {
 
-        if(txtEmail.getText().toString().equals("user@valid.com") && txtPassword.getText().toString().equals("testpass")){
-            // show a success msg
+       final String email = txtEmail.getText().toString();
+       String password = txtPassword.getText().toString();
 
-            Intent dashboardIntent = new Intent(this, DashboardActivity.class);
-            dashboardIntent.putExtra("UserEmail", txtEmail.getText().toString());
-            startActivity(dashboardIntent);
+       String url = "https://android.parthrai.ca/api/android/login";
 
-        }else{
-            // show an error msg
-            lblMsg.setText("Invalid User");
-        }
+
+
+        JSONObject payload = new JSONObject();
+
+        payload.put("email", email);
+        payload.put("password", password);
+
+
+        JSONArray arr_payload = new JSONArray();
+        arr_payload.put(payload);
+
+
+
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.POST, url, arr_payload, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+
+                if(response.length() == 0){
+                  lblMsg.setText("Please check your login details");
+                  return;
+                }
+
+
+                startDashboardIntent();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d(" Error - ", error.toString());
+            }
+        });
+
+        queue.add(request);
     }
 
     public void btnRegisterClick(View v){
 
         Intent registerIntent = new Intent(this, RegisterActivity.class);
         startActivity(registerIntent);
+    }
+
+    public void startDashboardIntent(){
+
+        Intent dashboardIntent = new Intent(this, DashboardActivity.class);
+        startActivity(dashboardIntent);
     }
 }
--- app/src/main/java/com/example/userapp/RegisterActivity.java
+++ app/src/main/java/com/example/userapp/RegisterActivity.java
@@ -3,12 +3,75 @@ package com.example.userapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    EditText edName;
+    EditText edEmail;
+    EditText edPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        edName = findViewById(R.id.txtName);
+        edEmail = findViewById(R.id.txtEmail);
+        edPassword = findViewById(R.id.txtPassword);
+
+    }
+
+
+    public void btnRegisterClick(View v){
+
+        String email = edEmail.getText().toString();
+        String name = edName.getText().toString();
+        String password = edPassword.getText().toString();
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        String url = "https://android.parthrai.ca/api/android/register";
+
+        JSONObject payload = new JSONObject();
+
+        try {
+            payload.put("email", email);
+            payload.put("name", name);
+            payload.put("password", password);
+            payload.put("dev", "parth");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        JsonObjectRequest register = new JsonObjectRequest(Request.Method.POST, url, payload, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+
+                Log.d(" Response --- ", response.toString());
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d(" ERROR --- ", error.toString());
+            }
+        });
+
+        queue.add(register);
+
+
     }
 }
